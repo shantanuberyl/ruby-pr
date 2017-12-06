f23e215cf584c4c558161e3301c943322ff1b5bf
shantanuberyl <shantanu.rana@berylsystems.com>
2017-12-06 12:59:27 +0530

    changes

--- local_variable.rb
+++ local_variable.rb
@@ -1,12 +1,14 @@
 class Static
   Var1 = 20
   Var2 = 50
+  Var3 = 70
   def print_var()
     puts "Value of 1st constant = #{Var1}"
     puts "Value of 2nd constant = #{Var2}"
+    puts "Value of 3rd constant = #{Var3}"
   end
 end
 
 #create and call object
 localVar = Static.new
-localVar.print_var()  
+localVar.print_var()
