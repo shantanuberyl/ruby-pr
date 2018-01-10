a07258669d0c9dcd47b7062af95070bc6d5c86c1
shantanuberyl <shantanu.rana@berylsystems.com>
2018-01-10 19:52:23 +0530

    Strings 10-01-2018

---
+++ strings.rb
@@ -0,0 +1,17 @@
+#simple string
+hometown = "this is my Hometown"
+puts "#{hometown}"
+
+puts "#{hometown.length}" #determining the length of the string
+
+puts "#{hometown.empty?}" #check if string is empty, returns true/false
+
+puts "#{"anyName".slice(2)}" #slice allows you to grab a single character of string
+
+puts "#{hometown.chars}" #convert the whole string in array
+
+puts "#{hometown.upcase}" #convert the string character in uppercase letters
+
+puts "#{hometown.downcase}" #convert the string character in lowercase letters
+
+puts "#{anyNames, center(19)}"
