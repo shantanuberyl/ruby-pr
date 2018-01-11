0cedc839594b89194556f0b17116dca65fd23998
shantanuberyl <shantanu.rana@berylsystems.com>
2018-01-11 11:23:41 +0530

    completed strings 11-01-2018

--- strings.rb
+++ strings.rb
@@ -14,4 +14,15 @@ puts "#{hometown.upcase}" #convert the string character in uppercase letters
 
 puts "#{hometown.downcase}" #convert the string character in lowercase letters
 
-puts "#{anyNames, center(19)}"
+fName = "philips"
+puts "#{fName}".center(41) #to add left and right padding to a string
+
+puts "#{fName}".ljust(21) #to add right padding to a string
+
+puts "#{fName}".rjust(21) #to add left padding to a string
+
+puts "#{fName}".include?("i") #checks if string contains another string
+
+puts "#{fName}".sub("i", "a") #replaces only first string
+
+puts "#{fName}".gsub("i", "a") #replaces all the string
