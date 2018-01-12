1941a7a98e7f1205b4c6e08fd3a943712667d6af
shantanuberyl <shantanu.rana@berylsystems.com>
2018-01-12 18:42:27 +0530

    methods and branching

--- udemy12-01-2018.rb
+++ udemy12-01-2018.rb
@@ -9,12 +9,45 @@ puts "Your first name has #{fName.length} characters"
 puts "Your last name has #{lName.length} characters"
 puts "your name in reverse #{fName.reverse} #{lName.reverse}"
 
+#################
+# Method and branching #
+
+def add(fNumber, sNumber)
+  fNumber.to_f + sNumber.to_f
+end
+
+def subtract(fNumber, sNumber)
+  fNumber.to_f - sNumber.to_f
+end
+
+def multiply(fNumber, sNumber)
+  fNumber.to_f * sNumber.to_f
+end
+
+def divide(fNumber, sNumber)
+  fNumber.to_f / sNumber.to_f
+end
+
+
 puts "enter first number :"
 fNumber = gets.chomp
 
 puts "enter second number :"
 sNumber = gets.chomp
 
-puts "#{fNumber} * #{sNumber} = #{fNumber.to_i * sNumber.to_i}"
-puts "#{fNumber} - #{sNumber} = #{fNumber.to_i - sNumber.to_i}"
-puts "#{fNumber} % #{sNumber} = #{fNumber.to_i % sNumber.to_i}"
+puts "Enter 1 to add, 2 to subtract, 3 to multiply, 4 to divide"
+selected = gets.chomp
+
+if selected == '1'
+  result = add(fNumber, sNumber)
+elsif selected == '2'
+  result = subtract(fNumber, sNumber)
+elsif selected == '3'
+  result = multiply(fNumber, sNumber)
+elsif selected == '4'
+    result = divide(fNumber, sNumber)
+else
+  puts "wrong input"
+end
+
+puts "Answer is #{result}"
